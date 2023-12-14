79617cbe45fd0d46a3b627df271c5b0c8bf9881f
zari786 <zaryababbasi786@gmail.com>
2023-12-14 11:02:05 +0500

    resolve minor bugs

--- CircularImage/build.gradle.kts
+++ CircularImage/build.gradle.kts
@@ -25,11 +25,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
@@ -57,15 +57,13 @@ publishing {
     }
 }
 
-/*
-afterEvaluate {
+/*afterEvaluate {
     publishing {
         publications {
             release(MavenPublication) {
-                from component.release
-
+                from components.release
                 groupId = 'com.github.zari786'
-                artificatId = 'circle-imageview'
+                artifactId = 'circle-imageview'
                 version = '1.0'
             }
         }
